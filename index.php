afc441f5311790856a3054981df318ecbc309dfe
famgz <famgz@proton.me>
2023-11-04 12:39:32 -0300

    finished frontend

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 include_once "header.php";
-include_once "index_action.php";
+include_once "index-action.php";
 ?>
-
 
 <div class="container border round mt-3 p-3 shadow bg-light">
     <div class="row">
         <h3><i class="fa-solid fa-cash-register"></i>Controle de Caixas</h3>
     </div>
-    
-    <!--  -->
+
+    <!-- Criar Novo Caixa -->
     <div class="row mt-3">
         <div class="col">
             <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#add-new-caixa">
@@ -25,14 +24,15 @@ include_once "index_action.php";
                     <span class="input-group-text" title="Buscar" id="btn-search">
                         <i class="fa-solid fa-magnifying-glass"></i>
                     </span>
-                    <input type="search" name="buscar" id="input-search" class="form-control" placeholder="Busca..." value="<?=$busca ?? ''; ?>">
+                    <input type="search" name="buscar" id="input-search" class="form-control" placeholder="Busca..."
+                        value="<?= $busca ?? ''; ?>">
                 </div>
             </form>
         </div>
         <div class="col">
             <form method="get" class="d-flex w-full justify-content-center" id="form-select-rpp">
                 <input type="hidden" name="p" value="<?= '1'; ?>">
-                <input type="hidden" name="busca" value="<?=$busca ?? ''; ?>">
+                <input type="hidden" name="busca" value="<?= $busca ?? ''; ?>">
                 <div class="input-group">
                     <span class="input-group-text">Mostrar</span>
 
@@ -61,9 +61,9 @@ include_once "index_action.php";
     <div class="row mt-3">
         <div class="col">
             <?php
-            if(!empty($_SESSION['msg'])) {
+            if (!empty($_SESSION['msg'])) {
                 echo $_SESSION['msg'];
-                unset($_SESSION ['msg']);
+                unset($_SESSION['msg']);
             }
             ?>
         </div>
@@ -114,7 +114,7 @@ include_once "index_action.php";
                 </thead>
                 <tbody>
                     <tr>
-                        <td>123</td>
+                        <td>#123</td>
                         <td>Caixa</td>
                         <td>R$ 1.234,56</td>
                         <td class="text-center">
@@ -123,7 +123,50 @@ include_once "index_action.php";
                             </a>
                         </td>
                         <td class="text-center">
-                            <i title="Excluir Caixa" class="fa-solid fa-trash-can action-icon icon-delete text-danger" data-bs-toggle="modal" data-bs-target="#delete caixa" data-id=""></i>
+                            <i title="Excluir Caixa" class="fa-solid fa-trash-can action-icon icon-delete text-danger"
+                                data-bs-toggle="modal" data-bs-target="#delete-caixa" data-id=""></i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#123</td>
+                        <td>Caixa</td>
+                        <td>R$ 1.234,56</td>
+                        <td class="text-center">
+                            <a title="Detalhes do Caixa" href="">
+                                <i class="fa-solid fa-circle-info action-icon icon-delete text-warning"></i>
+                            </a>
+                        </td>
+                        <td class="text-center">
+                            <i title="Excluir Caixa" class="fa-solid fa-trash-can action-icon icon-delete text-danger"
+                                data-bs-toggle="modal" data-bs-target="#delete-caixa" data-id=""></i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#123</td>
+                        <td>Caixa</td>
+                        <td>R$ 1.234,56</td>
+                        <td class="text-center">
+                            <a title="Detalhes do Caixa" href="">
+                                <i class="fa-solid fa-circle-info action-icon icon-delete text-warning"></i>
+                            </a>
+                        </td>
+                        <td class="text-center">
+                            <i title="Excluir Caixa" class="fa-solid fa-trash-can action-icon icon-delete text-danger"
+                                data-bs-toggle="modal" data-bs-target="#delete-caixa" data-id=""></i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#123</td>
+                        <td>Caixa</td>
+                        <td>R$ 1.234,56</td>
+                        <td class="text-center">
+                            <a title="Detalhes do Caixa" href="">
+                                <i class="fa-solid fa-circle-info action-icon icon-delete text-warning"></i>
+                            </a>
+                        </td>
+                        <td class="text-center">
+                            <i title="Excluir Caixa" class="fa-solid fa-trash-can action-icon icon-delete text-danger"
+                                data-bs-toggle="modal" data-bs-target="#delete-caixa" data-id=""></i>
                         </td>
                     </tr>
                 </tbody>
@@ -131,9 +174,86 @@ include_once "index_action.php";
         </div>
     </div>
 
+    <!-- Modal Criar Novo Caixa -->
+    <div class="modal fade" id="add-new-caixa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="add-caixa-action.php" method="post" class="needs-validation" novalidate>
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Criar Novo Caixa</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="nome" class="form-label">Nome do Caixa</label>
+                            <input type="text" name="nome" id="nome" class="form-control" required>
+                            <div class="invalid-feedback">O campo é obrigatório.</div>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label for="saldo-inicial" class="form-label">Saldo Inicial</label>
+                            <input type="text" name="saldo-inicial" id="saldo-inicial" class="form-control mask-value">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Confirma exclusão do Caixa -->
+    <div class="modal fade" id="delete-caixa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="delete-caixa-action.php" method="get">
+                    <input type="hidden" name="id" value="">
+
+                    <div class="modal-header bg-danger text-light">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">
+                            <i class="fa-solid fa-triangle-exclamation"></i> ATENÇÃO!
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Confirma a exclusão do caixa e todos os lançamentos vinculados?</p>
+                        <p>Importante: Esta ação não pode ser desfeita.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-danger">Confirmar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
 </div>
 
+<!-- Validação de campos obrigatórios -->
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+</script>
 
 
 <?php
