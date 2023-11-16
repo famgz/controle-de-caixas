3e53b77294f54ca9a5b1cb45e080eb688abdb48f
famgz <famgz@proton.me>
2023-11-16 16:00:09 -0300

    refactor: if else messages with single header/exit at the end

--- src/delete-caixa-action.php
+++ src/delete-caixa-action.php
@@ -7,7 +7,7 @@ $id = filter_input(INPUT_POST, "id", FILTER_VALIDATE_INT);
 debug($id);
 
 // Mensagem erro id inválido
-if(!$id) {
+if (!$id) {
     $_SESSION['msg'] = "
         <p class='alert alert-danger'>
             {$error_icon}
@@ -21,24 +21,23 @@ if(!$id) {
 $sql = $db->prepare("DELETE FROM caixas WHERE id = :id");
 $sql->bindValue(":id", $id);
 
-// Mensagem erro id não encontrado
-if(!$sql->execute()) {
+if (!$sql->execute()) {
+    // Mensagem erro id não encontrado
     $_SESSION['msg'] = "
-    <p class='alert alert-danger'>
-        {$error_icon}
-        Erro! Caixa #{$id} já excluído ou inexistente.
-    </p>
+        <p class='alert alert-danger'>
+            {$error_icon}
+            Erro! Caixa #{$id} já excluído ou inexistente.
+        </p>
     ";
-    header('Location: index.php');
-    exit;
+} else {
+    // Mensagem sucesso
+    $_SESSION['msg'] = "
+        <p class='alert alert-success'>
+            {$success_icon}
+            Caixa #{$id} excluído com sucesso!
+        </p>
+    ";
 }
 
-// Mensagem sucesso
-$_SESSION['msg'] = "
-    <p class='alert alert-success'>
-        {$success_icon}
-        Caixa #{$id} excluído com sucesso!
-    </p>
-";
 header('Location: index.php');
 exit;
