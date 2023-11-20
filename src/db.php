3ef388590095955797a9dd46317627aa78d720c9
famgz <famgz@proton.me>
2023-11-20 08:59:08 -0300

    database and table creation

--- src/db.php
+++ src/db.php
@@ -4,12 +4,31 @@ $host = 'localhost';
 $dbname = 'controle_caixas';
 $user = 'root';
 $pass = '';
+$sqlFile = "controle_caixas.sql";
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    // echo "Banco de dados conectado com sucesso!";
+    $db = new PDO("mysql:host=$host", $user, $pass);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Checar se database já existe
+    $databasesExistentes = $db->query("SHOW DATABASES")->fetchAll(PDO::FETCH_COLUMN);
+    if (in_array($dbname, $databasesExistentes)) {
+        // echo "Database já existe.\n";
+        $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        return;
+    }
+
+    // Criar database
+    $db->exec("CREATE DATABASE IF NOT EXISTS $dbname");
+
+    $db->exec("USE $dbname");
+
+    // Criar tables
+    $sql = file_get_contents($sqlFile);
+    $db->exec($sql);
+    // echo "Script SQL executado com sucesso\n";
+
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo "Erro: " . $e->getMessage() . "\n";
 }
-
-
